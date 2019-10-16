f9d5287eb622cbcc2c31f0aa21c793353b448f95
Andrew Aladjev <aladjev.andrew@gmail.com>
2019-10-16 13:35:49 +0300

    added basic imaginary extconf

---
+++ ext/extconf.rb
@@ -0,0 +1,68 @@
+# Ruby bindings for zstd library.
+# Copyright (c) 2019 AUTHORS, MIT License.
+
+require "mkmf"
+
+def require_header(name, types = [])
+  abort "Can't find #{name} header" unless find_header name
+
+  types.each do |type|
+    abort "Can't find #{type} type in #{name} header" unless find_type type, nil, name
+  end
+end
+
+require_header "zstd_errors.h", %w[ZSTD_ErrorCode]
+require_header "zstd.h", [
+  "ZSTD_CCtx *",
+  "ZSTD_DCtx *",
+  "ZSTD_strategy",
+  "ZSTD_cParameter",
+  "ZSTD_dParameter",
+  "ZSTD_inBuffer",
+  "ZSTD_outBuffer",
+  "ZSTD_CStream *",
+  "ZSTD_DStream *"
+]
+
+def require_library(name, functions)
+  functions.each do |function|
+    abort "Can't find #{function} function in #{name} library" unless find_library name, function
+  end
+end
+
+require_library(
+  "zstd",
+  %w[
+    ZSTD_getErrorCode
+    ZSTD_createCCtx
+    ZSTD_freeCCtx
+    ZSTD_createDCtx
+    ZSTD_freeDCtx
+    ZSTD_CCtx_setParameter
+    ZSTD_DCtx_setParameter
+    ZSTD_CStreamInSize
+    ZSTD_CStreamOutSize
+    ZSTD_DStreamInSize
+    ZSTD_DStreamOutSize
+    ZSTD_compressStream
+    ZSTD_flushStream
+    ZSTD_endStream
+    ZSTD_decompressStream
+  ]
+)
+
+extension_name = "zstds_ext".freeze
+dir_config extension_name
+
+# rubocop:disable Style/GlobalVars
+$srcs = %w[
+  main
+]
+.map { |name| "src/#{extension_name}/#{name}.c" }
+.freeze
+
+$CFLAGS << " -Wno-declaration-after-statement"
+$VPATH << "$(srcdir)/#{extension_name}:$(srcdir)/#{extension_name}/stream"
+# rubocop:enable Style/GlobalVars
+
+create_makefile extension_name
---
+++ ext/zstds_ext/main.c
@@ -0,0 +1,6 @@
+// Ruby bindings for zstd library.
+// Copyright (c) 2019 AUTHORS, MIT License.
+
+#include "ruby.h"
+
+void Init_zstds_ext() {}
