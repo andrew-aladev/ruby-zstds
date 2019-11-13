14cee1bd9976f7bf1a8441a46ef47b0370956791
Andrew Aladjev <aladjev.andrew@gmail.com>
2019-11-13 22:56:17 +0300

    updated scripts

--- scripts/push.sh
+++ scripts/push.sh
@@ -4,3 +4,4 @@ set -e
 cd "$(dirname $0)"
 
 git remote | xargs -n1 git push --all
+git remote | xargs -n1 git push --tags
--- scripts/update.sh
+++ scripts/update.sh
@@ -4,7 +4,7 @@ set -e
 cd "$(dirname $0)"
 
 git fetch --all || true
-git remote | xargs -n1 -I {} git rebase "{}/master" || true
+git remote | xargs -n1 -I {} git rebase "{}/$(git branch --show-current)" || true
 
 cd ".."
 rm -f "Gemfile.lock"
