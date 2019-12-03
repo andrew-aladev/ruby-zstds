5b46e115663680c910348c399093bf8c6c0652de
Andrew Aladjev <aladjev.andrew@gmail.com>
2019-12-04 02:19:59 +0300

    added test coverage workaround

--- test/coverage.rb
+++ test/coverage.rb
@@ -11,6 +11,22 @@ SimpleCov.formatters = SimpleCov::Formatter::MultiFormatter.new(
   ]
 )
 
+# Workaround for https://bugs.ruby-lang.org/issues/15980
+SimpleCov.module_eval do |mod|
+  def mod.start(profile = nil, &block)
+    require "coverage"
+
+    load_profile profile if profile
+    configure(&block) if block_given?
+
+    @result  = nil
+    @running = true
+    @pid     = Process.pid
+
+    Coverage.start :lines => true, :branches => true
+  end
+end
+
 SimpleCov.start do
   add_filter %r{^/test/}
 end
