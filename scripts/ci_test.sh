915e62d64c6179cb0c274b669e64cd20a7536ab0
Andrew Aladjev <aladjev.andrew@gmail.com>
2020-08-22 15:16:35 +0300

    updated travis and appveyor (running without test images)

--- scripts/ci_test.sh
+++ scripts/ci_test.sh
@@ -4,13 +4,11 @@ set -e
 DIR=$(dirname "${BASH_SOURCE[0]}")
 cd "$DIR"
 
-CPU_COUNT=$(grep -c "^processor" "/proc/cpuinfo" || sysctl -n "hw.ncpu")
-
 # This script is for CI machines only, it provides junk and changes some config files.
 # Please do not use it on your machine.
 
 TMP_PATH="$(pwd)/../tmp"
-TMP_SIZE="64"
+TMP_SIZE="16"
 
 ./temp/mount.sh "$TMP_PATH" "$TMP_SIZE"
 
@@ -28,41 +26,7 @@ fi
 bash -cl "\
   rvm use '.'; \
   gem install bundler && \
-  bundle install \
-"
-
-# Fix path environment params.
-export PATH="${PATH}:/usr/local/bin"
-export C_INCLUDE_PATH="${C_INCLUDE_PATH}:/usr/local/include"
-export LIBRARY_PATH="${C_INCLUDE_PATH}:/usr/local/lib"
-export LD_LIBRARY_PATH="${LD_LIBRARY_PATH}:/usr/local/lib"
-
-# Compiling library from source.
-ZSTD_BRANCH="v1.4.5"
-
-cd "tmp"
-
-# Remove orphaned directory.
-rm -rf "zstd"
-git clone "https://github.com/facebook/zstd.git" --single-branch --branch "$ZSTD_BRANCH" --depth 1 "zstd"
-cd "zstd"
-
-export CFLAGS="-DZSTD_MULTITHREAD=1"
-export CXXFLAGS="-DZSTD_MULTITHREAD=1"
-
-make clean
-make -j${CPU_COUNT}
-
-# "sudo" may be required for "/usr/local".
-if command -v sudo > /dev/null 2>&1; then
-  sudo make install
-else
-  make install
-fi
-
-bash -cl "\
-  cd ../.. && \
-  rvm use '.'; \
+  bundle install && \
   bundle exec rake clean && \
   bundle exec rake \
 "
