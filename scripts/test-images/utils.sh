b4c65955b7c43b11f9d0fc0774fc7896a8a99ff3
Andrew Aladjev <aladjev.andrew@gmail.com>
2020-09-15 00:14:58 +0300

    attached root cosmetics

--- scripts/test-images/utils.sh
+++ scripts/test-images/utils.sh
@@ -35,8 +35,8 @@ attach () {
 
   (
     container_root=$(mount "$container")
-    fusermount -zu attached_root || true
-    bindfs -r -o nonempty "${container_root}$1" attached_root
+    fusermount -zu attached-root || true
+    bindfs -r -o nonempty "${container_root}$1" attached-root
   ) || error=$?
 
   if [ ! -z "$error" ]; then
@@ -48,7 +48,7 @@ attach () {
 }
 
 detach () {
-  fusermount -zu attached_root || true
+  fusermount -zu attached-root || true
 
   unmount "$1" || true
   remove "$1" || true
