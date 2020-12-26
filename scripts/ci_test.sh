e7fa9dafe392cd668533d6534bb45363a71ee523
Andrew Aladjev <aladjev.andrew@gmail.com>
2020-12-27 01:28:34 +0300

    removed rvm workaround from ci test script

--- scripts/ci_test.sh
+++ scripts/ci_test.sh
@@ -11,17 +11,7 @@ TMP_SIZE="16"
 
 cd ".."
 
-ruby_version=$(< ".ruby-version")
-ruby_gemset=$(< ".ruby-gemset")
-
-if command -v "rvm" > /dev/null 2>&1; then
-  # Using latest available ruby version (search is based on required major version).
-  ruby_major_version=$(echo "${ruby_version%.*}" | sed "s/\./\\\./g") # escaping for regex
-  ruby_version=$(rvm list | grep -o -e "${ruby_major_version}\.[0-9]\+" | sort | tail -n 1)
-fi
-
 bash -cl "\
-  rvm use \"${ruby_version}@${ruby_gemset}\"; \
   gem install bundler && \
   bundle install && \
   bundle exec rake clean && \
