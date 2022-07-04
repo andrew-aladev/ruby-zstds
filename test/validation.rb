72ca4403d91aecdb8f149e8199e82bd677204a22
Andrew Aladjev <aladjev.andrew@gmail.com>
2022-07-04 19:12:05 +0300

    implementing adsp tests

--- test/validation.rb
+++ test/validation.rb
@@ -1,72 +1,14 @@
 # Ruby bindings for zstd library.
 # Copyright (c) 2019 AUTHORS, MIT License.
 
-require "stringio"
+require "adsp/test/validation"
 
 module ZSTDS
   module Test
     module Validation
-      NOOP_PROC = proc {} # no-op
+      include ADSP::Test::Validation
 
-      TYPES = [
-        nil,
-        1,
-        1.1,
-        "1",
-        true,
-        "a",
-        :a,
-        {},
-        [],
-        $stdout,
-        ::StringIO,
-        NOOP_PROC
-      ]
-      .freeze
-
-      INVALID_INTEGERS = (TYPES - [1]).freeze
-      INVALID_NUMBERS  = (TYPES - [1, 1.1]).freeze
-      INVALID_BOOLS    = (TYPES - [true]).freeze
-      INVALID_STRINGS  = (TYPES - %w[1 a]).freeze
-      INVALID_SYMBOLS  = (TYPES - %i[a]).freeze
-      INVALID_ARRAYS   = (TYPES - [[]]).freeze
-      INVALID_HASHES   = (TYPES - [{}]).freeze
-      INVALID_CHARS    = (INVALID_NUMBERS & INVALID_STRINGS).freeze
-
-      INVALID_POSITIVE_INTEGERS     = (INVALID_INTEGERS + [0, -1]).freeze
-      INVALID_NOT_NEGATIVE_INTEGERS = (INVALID_POSITIVE_INTEGERS - [0]).freeze
-      INVALID_DICTIONARIES          = TYPES
-
-      INVALID_ENCODINGS = %w[
-        ISO_8859_0
-        UTF-15
-        UTF-33
-      ]
-      .freeze
-
-      class StringIOWithoutEOF < ::StringIO
-        undef :eof?
-      end
-
-      class StringIOWithoutRead < ::StringIO
-        undef :read
-      end
-
-      class StringIOWithoutReadNonblock < ::StringIO
-        undef :read_nonblock
-      end
-
-      class StringIOWithoutReadpartial < ::StringIO
-        undef :readpartial
-      end
-
-      class StringIOWithoutWrite < ::StringIO
-        undef :write
-      end
-
-      class StringIOWithoutWriteNonblock < ::StringIO
-        undef :write_nonblock
-      end
+      INVALID_DICTIONARIES = TYPES
     end
   end
 end
--- test/version.test.rb
+++ test/version.test.rb
@@ -1,14 +1,15 @@
 # Ruby bindings for zstd library.
 # Copyright (c) 2019 AUTHORS, MIT License.
 
+require "adsp/test/version"
 require "zstds"
 
 require_relative "minitest"
 
 module ZSTDS
   module Test
-    class Version < Minitest::Test
-      def test_versions
+    class Version < ADSP::Test::Version
+      def test_version
         refute_nil ZSTDS::VERSION
         refute_nil ZSTDS::LIBRARY_VERSION
       end
